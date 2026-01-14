7ab16a7748c94ecb1d4b879115d674b0fc24b1f8
JoseA <joseantonioariza004@gmail.com>
2026-01-14 13:27:13 +0100

    commit 14/01

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -1,19 +1,27 @@
+
 import { useEffect, useState } from "react";
 import { taskService } from "../services/taskService";
 import type { Task } from "../types/Task";
 
 function TaskList() {
     const [tareas, setTareas] = useState<Task[]>([]);
+    const [cargando, setCargando] = useState(true);
 
     useEffect(() => {
-        taskService.getAll().then(listaTareas => setTareas(listaTareas));
+        taskService.getAll().then(listaTareas => {
+            setTareas(listaTareas);
+            setCargando(false);
+        });
     }, []);
 
     return (
         <ul>
-            {tareas && tareas.map(tarea => (
+            {cargando ? (
+                <li>Cargando...</li>
+            ) : (
+                tareas && tareas.map(tarea => (
                 <li key={tarea.id}>{tarea.title}</li>
-            ))}
+            )))}
         </ul>
     )
 }
